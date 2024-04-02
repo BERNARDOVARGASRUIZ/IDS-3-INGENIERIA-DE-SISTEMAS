19a65cd812ea2ef5077d3c0743a0da7faa386218
bernardo Vargas Ruiz <162154062+CodigoFuturo@users.noreply.github.com>
2024-04-02 18:44:21 -0400

    main.cpp

---
+++ Ejercicios/Ejercicios propuestos en la exposición/main.cpp
@@ -0,0 +1,8 @@
+/*
+  Este código está protegido por derechos de autor (c) 2024 Bernardo Vargas Ruiz.
+  Todos los derechos reservados.
+
+  Este código fuente es proporcionado como está y sin garantías de ninguna clase,
+  expresas o implícitas, incluyendo, pero no limitado a, garantías implícitas de
+  comerciabilidad, idoneidad para un propósito particular y no infracción.
+*/
