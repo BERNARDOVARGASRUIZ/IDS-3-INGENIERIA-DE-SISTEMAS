4b84973c6523972a74485f4ed509f0c45c26f668
bernardo Vargas Ruiz <162154062+CodigoFuturo@users.noreply.github.com>
2024-04-02 18:41:40 -0400

    Update ejercicio8.cpp

--- Ejercicios/Ejercicios propuestos en la exposición/ejercicio8.cpp
+++ Ejercicios/Ejercicios propuestos en la exposición/ejercicio8.cpp
@@ -1,24 +1,56 @@
+/*
+  Este código está protegido por derechos de autor (c) 2024 Bernardo Vargas Ruiz.
+  Todos los derechos reservados.
+*/
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> generar_serie(int numeroEntrada) {
+    vector<int> serie;
+    serie.push_back(numeroEntrada);
+
+    int i = 0;
+    do {
+        if (i == 0) {
+            string inver = to_string(numeroEntrada);
+            reverse(inver.begin(), inver.end());
+            int inverso = stoi(inver);
+            serie.push_back(inverso);
+        } else {
+            int siguiente = numeroEntrada + i;
+            string siguiente_str = to_string(siguiente);
+            serie.push_back(siguiente);
+
+            string inverso_siguiente_str = siguiente_str;
+            reverse(inverso_siguiente_str.begin(), inverso_siguiente_str.end());
+            int inverso_siguiente = stoi(inverso_siguiente_str);
+            serie.push_back(inverso_siguiente);
+        }
+        i++;
+    } while (i <= 4);
+    return serie;
+}
 
 int main() {
-  
-    vector<int> sn = {251, 152, 252, 252, 253, 352, 254, 452, 255, 552};
-    
-    cout << "LA SERIE DE NUMEROS ES:" <<endl;
-    for (int n : sn) {
-        std::cout << n << " ";
+    int numeroUsuario;
+    cout << "Ingrese un número: ";
+    cin >> numeroUsuario;
+    cout<<"======================================================================"<<endl;
+    vector<int> serieGenerada = generar_serie(numeroUsuario);
+    cout << "La serie generada es: ";
+    int suma = 0;
+    for (int i = 0; i < serieGenerada.size(); ++i) {
+        cout << serieGenerada[i];
+        if (i != serieGenerada.size() - 1) {
+            cout << ", ";
+        }
+        suma += serieGenerada[i];
     }
-    cout <<endl;
-    
-  
-    int s = 0;
-    for (int n : sn) {
-        s += n;
-    }
-    
-    cout << "LA SUMA DE LOS NUMEROS ES:" <<endl;
-    cout << s <<endl;
-
+    cout << endl;
+    cout << "La suma de la serie es: " << suma << endl;
     return 0;
 }
